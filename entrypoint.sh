c746cf1c1a0646d9e1cbb0d1a9a0c3669e0aa7b0
Jay Hofacker <jay@hofacker.us>
2018-05-26 18:47:30 -0400

    echo CMD parameters passed in

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "authorize account"
 b2 authorize-account "$B2_ACCOUNTID" "$B2_APPLICATIONKEY"
-echo "executing b2" $@
+echo "executing b2 " $@
 b2 $@
 echo "Press enter to exit"
 read
